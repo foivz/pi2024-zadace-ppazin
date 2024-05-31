34c8a10752c39fcb1f85521809fc12a95c19c5ab
Paula <ppazin22@student.foi.hr>
2024-05-31 11:28:41 +0200

    #Slučajno obrisan program.cs

---
+++ Software/RateYourPlate/RateYourPlate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DBLayer;
+
+namespace RateYourPlate
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            DB.SetConfiguration("PI2324_ppazin22_DB", "PI2324_ppazin22_User", "tR$*PV>_");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmPrijava());
+        }
+    }
+}
