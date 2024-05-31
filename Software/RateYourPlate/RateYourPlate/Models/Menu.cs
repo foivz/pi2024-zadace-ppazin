803f42014fe4c88bbc4c84045376be47e2089eb3
Paula <ppazin22@student.foi.hr>
2024-05-31 11:13:10 +0200

    #Napravljene klase za dnevne menije i detalje pojedinog menija

---
+++ Software/RateYourPlate/RateYourPlate/Models/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RateYourPlate
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public DateTime Datum { get; set; }
+        public int Menu1 { get; set; }
+        public int Menu2 { get; set; }
+        public int Menu3 { get; set; }
+    }
+}
---
+++ Software/RateYourPlate/RateYourPlate/Models/MenuDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RateYourPlate
+{
+    public class MenuDetails
+    {
+        public int Id { get; set; }
+        public string Juha { get; set; }
+        public string GlavnoJelo { get; set; }
+        public string Prilog { get; set; }
+        public string Desert { get; set; }
+
+    }
+}
